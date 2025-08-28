8dc1735ed1d1ca0f22eaa45542c3d11e6de2af8d
JamestheCog <jcog196013@gmail.com>
2025-08-28 12:46:33 +0800

    Changed requirements, edited prompts, and application constants

--- app.py
+++ app.py
@@ -2,15 +2,22 @@ import dotenv, os
 from flask import Flask, render_template
 from utils import logger
 from routes.conversation import conversation
-from cryptography import fernet
+from google import genai
+from google.genai import types
+from utils import chat
 
 dotenv.load_dotenv()
+bot_prompt = chat.load_base_prompt(os.getenv('FERNET_TOKEN'))
+chat_client = genai.Client(api_key = os.getenv('GEMINI_TOKEN'))
+chat_client = chat_client.chats.create(model = 'gemini-2.5-flash',
+                                       config = types.GenerateContentConfig(system_instruction = bot_prompt))
+
+# Set the application-specific constants:
 app = Flask(__name__)
-app.conversation_logger = logger.Logger()
-with open('./prompts/sic_bot_prompt.txt', 'rb') as prompt:
-    decryptor = fernet.Fernet(rf"{os.getenv('FERNET_TOKEN')}")
-    bot_prompt = decryptor.decrypt(prompt.read()).decode('utf-8')
 app.bot_prompt = bot_prompt
+app.conversation_logger = logger.Logger()
+app.chat_client = chat_client
+
 
 # Register the routes here plus define a starter route:
 app.register_blueprint(conversation)
