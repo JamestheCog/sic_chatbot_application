877c6b6ad2e8fce361b97e8daca6af2b1d47117f
JamestheCog <jcog196013@gmail.com>
2025-08-28 12:55:53 +0800

    Changed way messages are sent to the bot

--- routes/conversation.py
+++ routes/conversation.py
@@ -6,8 +6,23 @@ import dotenv, os
 from utils import chat, database
 from flask import Blueprint, current_app, request, jsonify
 
+# Initialize Google Gemini here:
+from google import genai
+from google.genai import types
+from cryptography.fernet import Fernet
+
 dotenv.load_dotenv()
 conversation = Blueprint('conversation', __name__, template_folder = 'templates')
+
+'''
+database.store_message(data['content'], 'user', current_app.conversation_logger.id)
+current_app.conversation_logger.log_message(data['content'], 'user')
+response = chat.respond_with_gemini(data['content'], current_app.bot_prompt)
+# response = chat.respond(current_app.conversation_logger.message_history, current_app.bot_prompt)
+current_app.conversation_logger.log_message(response, 'assistant')
+database.store_message(response, 'assistant', current_app.conversation_logger.id)
+return(jsonify({'content' : response}), 200)
+'''
 
 # Define the routes here...
 @conversation.route('/send_message', methods = ['POST'])
@@ -20,11 +35,9 @@ def send_message():
         return(jsonify({'error_message' : 'Invalid data sent', 'error_code' : 500,
                         'user_message' : 'ERROR: SOMETHING HAPPENED'}), 500)
     database.store_message(data['content'], 'user', current_app.conversation_logger.id)
-    current_app.conversation_logger.log_message(data['content'], 'user')
-    response = chat.respond(current_app.conversation_logger.message_history, current_app.bot_prompt)
-    current_app.conversation_logger.log_message(response['content'], 'assistant')
-    database.store_message(response['content'], 'assistant', current_app.conversation_logger.id)
-    return(jsonify({'content' : response.get('content')}), 200)
+    response = current_app.chat_client.send_message(data['content'])
+    database.store_message(response.text, 'assistant', current_app.conversation_logger.id)
+    return(jsonify({'content' : response.text}), 200)
 
 @conversation.route('/reset_conversation', methods = ['POST'])
 def reset_conversation():
@@ -32,5 +45,10 @@ def reset_conversation():
     Reset the conversation and by extension, the logger as part of the 
     current_app object's attributes.
     '''
+    chat_client = genai.Client(api_key = os.getenv('GEMINI_TOKEN'))
+    chat_client = chat_client.chats.create(model = 'gemini-2.5-flash',
+                                           config = types.GenerateContentConfig(
+                                               system_instruction = chat.load_base_prompt(os.getenv('FERNET_TOKEN'))))
+    current_app.chat_client = chat_client
     current_app.conversation_logger.reset()
     return(jsonify({'status' : 200}), 200)
