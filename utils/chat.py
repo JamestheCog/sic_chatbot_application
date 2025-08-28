b5e1e387ab465cedfc016db472febce97e18f00b
JamestheCog <jcog196013@gmail.com>
2025-08-28 12:48:17 +0800

    Added utility for loading base prompt and more

--- utils/chat.py
+++ utils/chat.py
@@ -3,8 +3,16 @@ A module to contain helper functions for responding to user inputs.
 '''
 
 import os, requests, dotenv
+from cryptography import fernet
+
+# Dependencies for Google
+from google import genai
+from google.genai import types
 
 def respond(user_message_history, base_prompt):
+    '''
+    NOTE (Thursday, August 28th, 2025): this function is NOT currently being used.
+    '''
     dotenv.load_dotenv()
     API_URL = 'https://openrouter.ai/api/v1/chat/completions'
     HEADER = {'Authorization' : f"Bearer {os.getenv('OPENROUTER_TOKEN')}", 'Content-Type' : 'application/json'}
@@ -20,3 +28,28 @@ def respond(user_message_history, base_prompt):
     else:
         message = message['choices'][0]['message']
     return(message)
+
+def respond_with_gemini(message, base_prompt):
+    '''
+    Does the same thing as the respond() function, but uses Gemini instead (since Gemini has a pretty generous
+    limit for free-tier users like us)
+
+    NOTE (Thursday, 28th August) --> this function is not being used in the time being!
+    '''
+    dotenv.load_dotenv()
+    chat_client = genai.Client(api_key = os.getenv('GEMINI_TOKEN'))
+    chat = chat_client.chats.create(model = 'gemini-2.5-flash', 
+                                    config = types.GenerateContentConfig(system_instruction = base_prompt))
+    response = chat.send_message(message)
+    return(response.text)
+
+def load_base_prompt(fernet_key):
+    '''
+    Load in the base prompt that Gemini depends on to function as the bot:
+    '''
+    decryptor = fernet.Fernet(rf'{fernet_key}')
+    with open('./prompts/sic_bot_prompt.txt', 'rb') as prompt:
+        decrypted = decryptor.decrypt(prompt.read()).decode('utf-8')
+    return(decrypted)
+
+    
