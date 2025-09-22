81baea3ad8ac9ca9e3e263fc7cbc6aeed03a5d13
JamestheCog <jcog196013@gmail.com>
2025-09-22 14:27:51 +0800

    Fixed bug with conversation continuation

--- app.py
+++ app.py
@@ -7,14 +7,10 @@ from utils import chat
 
 dotenv.load_dotenv()
 bot_prompt = chat.load_base_prompt(os.getenv('FERNET_TOKEN'))
-chat_client = genai.Client(api_key = os.getenv('GEMINI_TOKEN'))
-chat_client = chat_client.chats.create(model = 'gemini-2.5-flash',
-                                       config = types.GenerateContentConfig(system_instruction = bot_prompt))
 
 # Set the application-specific constants:
 app = Flask(__name__)
 app.bot_prompt = bot_prompt
-app.chat_client = chat_client
 app.secret_key = str(uuid.uuid4())
 
 # Register the routes here plus define a starter route:
--- routes/conversation.py
+++ routes/conversation.py
@@ -5,6 +5,8 @@ A module to contain helper functions related to the Chatbot's functionality.
 import dotenv, os, time, random, uuid
 from utils import chat, database, logger
 from flask import Blueprint, current_app, request, jsonify, session
+from google import genai
+from google.genai import types
 
 # Initialize Google Gemini here:
 from google import genai
@@ -12,16 +14,20 @@ from google.genai import types
 
 dotenv.load_dotenv()
 conversation = Blueprint('conversation', __name__, template_folder = 'templates')
-loggers = {}
+items = {}
 
 # ===
-def get_logger():
+def initialize_items():
     if 'session_id' not in session:
-        session['session_id'] = str(uuid.uuid4())
+        items['session_id'] = str(uuid.uuid4())
+    if 'chat_assistant' not in session:
+        chat_client = genai.Client(api_key = os.getenv('GEMINI_TOKEN'))
+        items['chat_assistant'] = chat_client.chats.create(model = 'gemini-2.5-flash', 
+                                                             config = types.GenerateContentConfig(system_instruction = current_app.bot_prompt))
     session_id = session.get('session_id')
-    if session_id not in loggers:
-        loggers[session_id] = logger.Logger()
-    return(loggers[session_id])
+    if session_id not in items:
+        items[session_id] = logger.Logger()
+    return(items[session_id], items['chat_assistant'])
 # ===
 
 # Define the routes here...
@@ -30,14 +36,14 @@ def send_message():
     '''
     Generate a response plus log the user's message.
     '''
-    data, retry_times, user_logger = request.json, 0, get_logger() 
-    print(data)
+    data, retry_times = request.json, 0
+    user_logger, chat_client = initialize_items()
     if not isinstance(data, dict):
         return(jsonify({'error_message' : 'Invalid data sent', 'error_code' : 500,
                         'user_message' : 'ERROR: SOMETHING HAPPENED'}), 500)
     database.store_message(data['content'], 'user', user_logger.id)
     while retry_times < int(os.getenv('RETRY_RESPONSES')):
-        response = current_app.chat_client.send_message(data['content'])
+        response = chat_client.send_message(data['content'])
         if isinstance(response, dict):
             if response.get('error') is None:
                 retry_times += 1
